e5321b6097741c042f0938cadd237c55c6f0a083
siddharth gandhi <siddharthgandhi@outlook.com>
2022-12-16 22:56:18 +0530

    [Siddharth]Add:Cab Invoice Generator App Problem

---
+++ CabInvoiceGeneratorApp/CabInvoiceGeneratorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabInvoiceGeneratorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Invoice Generator App Problem Statements");
+        }
+    }
+}
